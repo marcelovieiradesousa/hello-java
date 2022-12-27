56268e1dc30fb39ba7e156c9a52408e90b4f1fc4
Marcelo Vieira <marcelovieira.tec@gmail.com>
2022-12-27 10:46:34 -0300

    Implementa formas de MELHORAR as operações lógicas

--- src/edu/marcelo/sintaxe/Operadores.java
+++ src/edu/marcelo/sintaxe/Operadores.java
@@ -4,13 +4,35 @@ public class Operadores {
   //Material de Estudo
   //https://felipe-silva-aguiar.gitbook.io/dio-java/gitbook/sintaxe/operadores
   public static void main(String[] args) {
+    //Unários
     int numero = 0;
     System.out.println(numero--);
     System.out.println(numero);System.out.println(--numero);
 
+    //Relacionais
     if(numero < 0.3){
       System.out.println("numero menor que 0.3");
     }
+
+
+    //OPERADORES RELACIONAIS + LOGICOS
+    double salarioMensal = 1320.50d;
+    double mediaSalario = 2023.50d;
+    int quantidadeDependentes = 4;
+    int mediaDependentes = 2;
+
+    /*MODO: hard */
+    System.out.println((salarioMensal < mediaSalario) && (quantidadeDependentes >= mediaDependentes));
+
+    /*MODO: medium */
+    boolean salarioBaixo = salarioMensal < mediaSalario;
+    boolean muitosDependentes = quantidadeDependentes >= mediaDependentes;
+    System.out.println(salarioBaixo && muitosDependentes);
+
+    /*MODO: eeeeasy */
+    boolean recebeAuxilio = (salarioBaixo) && (muitosDependentes);
+    System.out.println("Recebe auxílio? " + recebeAuxilio);
+
   }
 
 }

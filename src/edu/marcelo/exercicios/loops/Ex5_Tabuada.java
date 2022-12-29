8b096e94f7bb3c96ff5f26babf3f42e2fb3df104
Marcelo Vieira <marcelovieira.tec@gmail.com>
2022-12-29 11:00:30 -0300

    Recolocado codigo Fatorial na Class Ex6_Fatorial

--- src/edu/marcelo/exercicios/loops/Ex5_Tabuada.java
+++ src/edu/marcelo/exercicios/loops/Ex5_Tabuada.java
@@ -2,22 +2,18 @@ package edu.marcelo.exercicios.loops;
 
 import java.util.Scanner;
 /*
-Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
-Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
+Desenvolva um gerador de tabuada,
+capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
+O usuário deve informar de qual numero ele deseja ver a tabuada.
+A saída deve ser conforme o exemplo abaixo:
+Tabuada de 5:
+5 X 1 = 5
+5 X 2 = 10
+...
+5 X 10 = 50
 */
-
 public class Ex5_Tabuada {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Digite um fatorial: ");
-    int fat = scanner.nextInt();
-    int result = 1;
-    System.out.print(fat + "! = ");
-    while(fat > 1){
-      result = result * fat;
-      --fat; 
-    }
-    System.out.print(result);
   }
 }
--- src/edu/marcelo/exercicios/loops/Ex6_Fatorial.java
+++ src/edu/marcelo/exercicios/loops/Ex6_Fatorial.java
@@ -1,5 +1,23 @@
 package edu.marcelo.exercicios.loops;
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+Ex.: 5!= 120 (5 X 4 X 3 X 2 X 1)
+*/
+
+import java.util.Scanner;
 
 public class Ex6_Fatorial {
-  
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Digite um fatorial: ");
+      int fat = scanner.nextInt();
+      int result = 1;
+      System.out.print(fat + "! = ");
+      while(fat > 1){
+        result = result * fat;
+        --fat; 
+      }
+      System.out.print(result);
+    }
 }

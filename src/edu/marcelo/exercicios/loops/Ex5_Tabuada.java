9c7b6cbcf3a5097d7336ea2fd40c75b5c980f5e2
Marcelo Vieira <marcelovieira.tec@gmail.com>
2022-12-29 11:23:42 -0300

    Tabuada done

--- src/edu/marcelo/exercicios/loops/Ex5_Tabuada.java
+++ src/edu/marcelo/exercicios/loops/Ex5_Tabuada.java
@@ -14,6 +14,14 @@ Tabuada de 5:
 */
 public class Ex5_Tabuada {
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite um numero de 1 a 10 para saber sua tabuada: ");
+    int num = scanner.nextInt();
 
+    //5 x i = result
+    for(int i = 1; i < 11; i++){
+      int result = num * i;
+      System.out.println(String.format("%s x %s = %s", num, i, result)); //interpolação de string!
+    }
   }
 }

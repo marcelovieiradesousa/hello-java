205583d8c08f99c38713afbb8915d7d96e859fab
Marcelo Vieira <marcelovieira.tec@gmail.com>
2022-12-29 11:56:05 -0300

    Consoantes done

--- src/edu/marcelo/exercicios/arrays/Ex2_Consoantes.java
+++ src/edu/marcelo/exercicios/arrays/Ex2_Consoantes.java
@@ -1,5 +1,39 @@
 package edu.marcelo.exercicios.arrays;
+/*
+Faça um Programa que leia um vetor de 6 caracteres,
+e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+*/
 
 public class Ex2_Consoantes {
-  
+  public static void main(String[] args) {
+    String [] vetor = {"m","a","r","c","e","l","o"};
+    int qtdConsoante = 0;
+
+    for(int i = 0; i < vetor.length; i++){
+      switch(vetor[i]){
+        case "a": 
+          qtdConsoante++;
+          System.out.println("Consoante: "+vetor[i]);
+          break;
+        case "e": 
+          qtdConsoante++;
+          System.out.println("Consoante: "+vetor[i]);
+          break;
+        case "i":
+          qtdConsoante++;
+          System.out.println("Consoante: "+vetor[i]);
+          break;
+        case "o":
+          qtdConsoante++;
+          System.out.println("Consoante: "+vetor[i]);
+          break;
+        case "u":
+          qtdConsoante++;
+          System.out.println("Consoante: "+vetor[i]);
+          break;
+      }
+    }
+    System.out.println("Quantidade de Consoantes: "+qtdConsoante);
+  }
 }

de1837b658f902e5cfd77b44f34b339ad02837aa
Marcelo Vieira <marcelovieira.tec@gmail.com>
2022-12-29 11:38:22 -0300

    OrdemInversa done

--- src/edu/marcelo/exercicios/arrays/Ex1_OrdemInversa.java
+++ src/edu/marcelo/exercicios/arrays/Ex1_OrdemInversa.java
@@ -1,5 +1,15 @@
 package edu.marcelo.exercicios.arrays;
-
+/*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
 public class Ex1_OrdemInversa {
-  
+  public static void main(String[] args) {
+    int [] vetor = {4,6,9,8,5,4};
+    int index = vetor.length - 1; 
+
+    for(int i = -1; i < index; index--){
+      System.out.println(String.format("Ordem inversa do vetor |  Índice %s - Valor: %s", index, vetor[index]));
+    }
+  } 
 }

c216c45d61b671abc3815d7bbe4f3c4b02fe2f58
Marcelo Vieira <marcelovieira.tec@gmail.com>
2023-01-12 12:37:07 -0300

    Map: PopulacaoNordeste done

--- src/edu/marcelo/collections/map/PopulacaoNordeste.java
+++ src/edu/marcelo/collections/map/PopulacaoNordeste.java
@@ -1,5 +1,8 @@
 package edu.marcelo.collections.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PopulacaoNordeste {
   /*
    * Dada a população estimada de alguns estados do NE brasileiro, faça:
@@ -21,4 +24,25 @@ public class PopulacaoNordeste {
    * 
    * Exiba a população PE;
    */
+  public static void main(String[] args) {
+    Map<String, Integer> regiaoNordeste = new HashMap<>();
+
+    System.out.println("Crie um dicionário e relacione os estados e suas populações");
+    regiaoNordeste.put("PE", 9616621);
+    regiaoNordeste.put("AL", 3351543);
+    regiaoNordeste.put("CE", 9187103);
+    regiaoNordeste.put("RN", 3534265);
+    System.out.println(regiaoNordeste);
+
+    System.out.println("Substitua a população do estado do RN por 3.534.165");
+    regiaoNordeste.put("RN", 3534165);
+    System.out.println("População de RN: " + regiaoNordeste.get("RN"));
+
+    System.out.println("Confira se o estado PB está no dicionário, caso não adicione: PB - 4.039.277");
+    System.out.println("Há o estado de PB? " + regiaoNordeste.containsKey("PB"));
+    regiaoNordeste.put("PB", 4039277);
+
+    System.out.println("Exiba a população PE");
+    System.out.println(regiaoNordeste.get("PB"));
+  }
 }
